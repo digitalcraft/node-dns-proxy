fb98f66708f0bd33b9e49979ed8f2db5164aedf7
Jeff Lynne <jeff@digital8.com.au>
2014-03-17 13:37:41 +1000

    update regex - thanks shorty

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ proxy.createServer({
       "roflware.com": "123.234.123.234"
     },
     filters: {
-        "/*.docker.digital8.com.au/": "10.20.0.10",
-        "/*.vm/": "10.20.0.10",
-        "/*.dev/": "127.0.0.1",
-        "/*.local/": "127.0.0.1"
+        "\.docker.digital8.com.au$": "10.20.0.10",
+        "\.vm$": "10.20.0.10",
+        "\.dev$": "127.0.0.1",
+        "\.local$": "127.0.0.1"
     },
     cache: true
 }).start();
